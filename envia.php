7d909150d086ef843ad93afb47677b2780bed5c0
rafaelsoaresgw <149423227+rafaelsoaresgw@users.noreply.github.com>
2024-12-24 15:16:34 -0300

    Update envia.php

--- envia.php
+++ envia.php
@@ -1,24 +1,27 @@
 <?php
-$nome = addslashes($_POST['nome']);
-$email = addslashes($_POST['email']);
-$celular = addslashes($_POST['celular']);
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $nome = addslashes($_POST['nome']);
+    $email = addslashes($_POST['email']);
+    $celular = addslashes($_POST['celular']);
+    $mensagem = addslashes($_POST['mensagem']);
 
+    $para = "rafaelsoaresgw@gmail.com";
+    $assunto = "Coleta de dados - Soares";
 
-$para = "rafaelsoaresgw@gmail.com";
-$assunto = "Coleta de dados - Soares";
+    $corpo = "Nome: " . $nome . "\n" .
+             "Email: " . $email . "\n" .
+             "Celular: " . $celular . "\n" .
+             "Mensagem: " . $mensagem;
 
-$corpo = "Nome: ".$Nome."\n"."Email: ".$email."\n"."Celular: ".$celular;
+    $cabeca = "From: rafaelsoaresgw2003@gmail.com" . "\n" .
+              "Reply-To: " . $email . "\n" .
+              "X-Mailer: PHP/" . phpversion();
 
-$cabeca ="From: rafaelsoaresgw2003@gmail.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversion();
-
-if(mail($para,$assunto,$corpo,$cabeca)){
-    echo("E-mail enviado com sucesso!");
+    if (mail($para, $assunto, $corpo, $cabeca)) {
+        echo "E-mail enviado com sucesso!";
+    } else {
+        echo "Houve um erro ao enviar o e-mail.";
+    }
 }
-
-else{
-   
-    echo("Houve um erro ao enviar o E-mail");
-
-}
-
 ?>
+
